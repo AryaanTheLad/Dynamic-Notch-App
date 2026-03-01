097a4d1d35db67a95c1964d085639cb45b79b9c5
Aryaan Qaiser <makhdoomaryaan786@gmail.com>
2026-03-01 14:25:32 +0500

    vercel deployment fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import Hero from './components/Hero';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
@@ -21,8 +19,6 @@ function App() {
         <SpeedInsights />
         < Analytics />
       </main>
-      <SpeedInsights />
-      <Analytics />
     </div>
   );
 }
